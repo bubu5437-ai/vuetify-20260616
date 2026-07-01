5865e01ce5eaac061499c4017ef50a5a8de8e1f4
bubu5437-ai <bubu5437@gmail.com>
2026-07-01 15:30:07 +0800

    上傳

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cpq0oel4uis"
+    "revision": "0.n6noe0pcpg8"
   }], {
     "ignoreURLParametersMatching": [/.*/]
   });
